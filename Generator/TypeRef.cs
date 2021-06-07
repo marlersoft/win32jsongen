9656e48e78c5f6a2c8380cf14ae84a4c1c367224
Jonathan Marler <johnnymarler@gmail.com>
2021-06-07 15:06:28 -0600

    uprade to 10.2.84-preview

--- Generator/TypeRef.cs
+++ Generator/TypeRef.cs
@@ -34,12 +34,12 @@ namespace JsonWin32Generator
 
             if (this is TypeRef.User userType)
             {
-                if (userType.Info.Fqn == "Windows.Win32.System.SystemServices.PSTR")
+                if (userType.Info.Fqn == "Windows.Win32.Foundation.PSTR")
                 {
                     return TypeRef.Primitive.Byte;
                 }
 
-                if (userType.Info.Fqn == "Windows.Win32.System.SystemServices.PWSTR")
+                if (userType.Info.Fqn == "Windows.Win32.Foundation.PWSTR")
                 {
                     return TypeRef.Primitive.Char;
                 }
