79768fbdd6095fbf144276534e32cc0559df65b6
Jonathan Marler <johnnymarler@gmail.com>
2024-02-21 16:47:06 -0700

    include a version.txt file in the output

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
+using System.Text;
 using JsonWin32Generator;
 
 internal class Program
@@ -17,12 +18,20 @@ internal class Program
         string repoDir = JsonWin32Common.FindWin32JsonRepo();
         string apiDir = JsonWin32Common.GetAndVerifyWin32JsonApiDir(repoDir);
         CleanDir(apiDir);
+        string versionFile = Path.Combine(repoDir, "version.txt");
+        File.Delete(versionFile);
         var generateTimer = Stopwatch.StartNew();
         {
             using FileStream metadataFileStream = File.OpenRead(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location!)!, "Windows.Win32.winmd"));
             using PEReader peReader = new PEReader(metadataFileStream);
             Console.WriteLine("OutputDirectory: {0}", apiDir);
             JsonGenerator.Generate(peReader.GetMetadataReader(), apiDir);
+        }
+
+        {
+            using var fileStream = new FileStream(versionFile, FileMode.Create, FileAccess.Write, FileShare.Read);
+            using var streamWriter = new StreamWriter(fileStream, new UTF8Encoding(false));
+            streamWriter.Write(MetadataVersion.Value);
         }
 
         Console.WriteLine("Generation time: {0}", generateTimer.Elapsed);
